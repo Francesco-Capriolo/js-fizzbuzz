608301ee052ad1b0e63bec0a5fb3827529f942f2
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-02-24 17:52:14 +0100

    completed for and if with function

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-const wrapper = document.getElementById("ms-wrapper");
+function myFunction() {
+    const wrapper = document.getElementById("ms-wrapper");
 
-for (let i = 0; i < 30; i++) {
-    /* creo il div di riferimento */
-    let newDiv = document.createElement("div");
-    /* aggiungo al div appena creato le classi necessarie */
-    newDiv.classList.add("col-12", "col-sm-9", "col-md-6", "col-lg-3", "ms-box", "bg-warning", );
-    /* cambio il contenuto del div appena creato inserendo un valore */
-
-    /* aggiungo il div al parent */
-    wrapper.append(newDiv);
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-        newDiv.classList.add("ms-bg-fizzbuzz");
-        console.log("frizzbuzz");
-    } else if (i % 3 == 0) {
-        newDiv.classList.add("ms-bg-fizz");
-        console.log("frizz");
-    } else if (i % 5 == 0) {
-        newDiv.classList.add("ms-bg-buzz");
-        console.log("buzz");
-    } else {
-        newDiv.innerHTML = "nope";
-        console.log(i);
+    for (let i = 1; i <= 100; i++) {
+        /* creo il div di riferimento */
+        let newDiv = document.createElement("div");
+        /* aggiungo al div appena creato le classi necessarie */
+        newDiv.classList.add("col-12", "col-sm-8", "col-md-4", "col-lg-1", "ms-box", "bg-primary", );
+        /* cambio il contenuto del div appena creato inserendo un valore */
+        /* aggiungo il div al parent */
+        wrapper.append(newDiv);
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            newDiv.classList.add("ms-bg-fizzbuzz");
+            console.log("frizzbuzz");
+        } else if (i % 3 == 0) {
+            newDiv.classList.add("ms-bg-fizz", "text-white");
+            newDiv.innerHTML = "Fizz";
+            console.log("frizz");
+        } else if (i % 5 == 0) {
+            newDiv.classList.add("ms-bg-buzz", "text-white");
+            newDiv.innerHTML = "Buzz";
+            console.log("buzz");
+        } else {
+            newDiv.innerHTML = "Nope";
+            console.log(i);
+        }
     }
 }
